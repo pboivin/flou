0e20bd7d66028fc71d046941bbe6cfe130391948
Patrick Boivin <pboivin@gmail.com>
2018-09-25 17:41:14 -0400

    Basic structure for main class

--- Flou.php
+++ Flou.php
@@ -1,6 +1,41 @@
 <?php
 
-class Flou
-{
-    // Fresh start
+class Flou {
+    private $base_path;
+    private $base_url;
+    private $original_file;
+    private $processed_file;
+
+    public function __constructor() {
+    }
+
+    public function load($original_file) {
+    }
+
+    public function process($processed_file=null) {
+    }
+
+    public function isProcessed() {
+    }
+
+    public function setBasePath($base_path) {
+    }
+
+    public function setBaseUrl($base_url) {
+    }
+
+    public function getHTML($original_url=null, $processed_url=null) {
+    }
+
+    public function getOriginalFile() {
+    }
+
+    public function getProcessedFile() {
+    }
+
+    public function getOriginalURL() {
+    }
+
+    public function getProcessedURL() {
+    }
 }
