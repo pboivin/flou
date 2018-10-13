fcd164507c71535028cab6b33a0c0a39168e2520
Patrick Boivin <pboivin@gmail.com>
2018-10-13 15:54:49 -0400

    Add support for complete file path in load()

--- Flou/Image.php
+++ Flou/Image.php
@@ -13,9 +13,18 @@ class Image {
     }
 
     public function load($original_file) {
-        $file_path = Path::join($this->base_path, $original_file);
-        Path::checkFile($file_path);
-        $this->original_file = $original_file;
+        if ($this->base_path) {
+            // original_file is to be found in base_path
+            $file_path = Path::join($this->base_path, $original_file);
+            Path::checkFile($file_path);
+            $this->original_file = $original_file;
+        } else {
+            // original_file is a complete path, extract base_path from it
+            $file_path = $original_file;
+            Path::checkFile($file_path);
+            $this->base_path = dirname($file_path);
+            $this->original_file = basename($file_path);
+        }
         return $this;
     }
 
@@ -33,6 +42,7 @@ class Image {
     }
 
     public function setBaseUrl($base_url) {
+        $this->base_url = $base_url;
         return $this;
     }
 
--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -12,10 +12,16 @@ class ImageTest extends TestCase {
         self::$base_path = Path::join($current_dir, "fixtures");
     }
 
-    public function testLoad() {
+    public function testLoadWithBasePath() {
         $image = (new Flou\Image())
             ->setBasePath(self::$base_path)
             ->load("image1.jpg");
+        $this->assertNotNull($image->getOriginalFilePath());
+    }
+
+    public function testLoadFullPath() {
+        $path = Path::join(self::$base_path, "image1.jpg");
+        $image = (new Flou\Image())->load($path);
         $this->assertNotNull($image->getOriginalFilePath());
     }
 }
