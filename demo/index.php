e6c8e31aab7d52a9e468e8b26e483c4d200a5ad5
Patrick Boivin <pboivin@gmail.com>
2018-10-16 13:55:36 -0400

    Update demo

--- demo/index.php
+++ demo/index.php
@@ -1,12 +1,16 @@
 <?php
 
-require_once "../ProgressiveImages.php";
-ProgressiveImages::init([
-    "blur_path" => "img/blur",
-    "blur_url" => "img/blur",
-    "base_path" => "img",
-    "base_url" => "img",
-]);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$image1 = (new Flou\Image())
+    ->setBasePath(__DIR__ . "/img")
+    ->setBaseURL("./img")
+    ->load("image1.jpg")
+    ->process();
 
 ?>
 <!doctype html>
@@ -15,20 +19,28 @@ ProgressiveImages::init([
     <meta charset="utf-8">
     <title>Demo</title>
     <link rel="stylesheet" href="main.css">
+    <script>
+        window.handleFlouImageLoaded = function(image) {
+            image.parentElement.classList.add("is-loaded");
+        }
+    </script>
 </head>
 <body>
     <div class="container">
         <h1>Demo</h1>
 
-        <!--
-        <div>
-            <h2>Original image</h2>
-            <?= ProgressiveImages::getOriginal("/img/image1.jpg", "demo-image") ?>
-        </div>
-        -->
-
-        <div>
-            <?= ProgressiveImages::generate("/img/image1.jpg", "demo-image") ?>
+        <div class="flou-container">
+            <img
+                class="flou-processed-image"
+                src="<?= $image1->getProcessedURL() ?>"
+                alt=""
+            />
+            <img
+                class="flou-original-image"
+                src="<?= $image1->getOriginalURL() ?>"
+                alt="My Vacation Photo"
+                onload="handleFlouImageLoaded(this)"
+            />
         </div>
     </div>
 </body>
