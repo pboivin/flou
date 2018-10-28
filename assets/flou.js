7c3ab20a75bff21f93c11d4849bdc6171ba6669d
Patrick Boivin <pboivin@gmail.com>
2018-10-28 10:51:01 -0400

    Add image_class and image_loaded_class static properties

--- assets/flou.js
+++ assets/flou.js
@@ -4,18 +4,21 @@
     else { root.Flou = factory(); }
 }(typeof self !== 'undefined' ? self : this, function () {
     var Flou = {
-        IMAGE_LOADED_DELAY: 500,
+        image_class: "flou-image",
+        image_loaded_class: "flou-image-loaded",
+        image_loaded_delay: 500,
 
         handleImageLoaded: function(src) {
             var image = this;
             setTimeout(function(){
                 image.setAttribute("src", src);
-                image.classList.add("flou-image-loaded");
-            }, Flou.IMAGE_LOADED_DELAY);
+                image.classList.add(Flou.image_loaded_class);
+            }, Flou.image_loaded_delay);
         },
 
         loadImages: function() {
-            var images = document.querySelectorAll(".flou-image");
+            var selector = "." + Flou.image_class + ":not(." + Flou.image_loaded_class + ")";
+            var images = document.querySelectorAll(selector);
 
             [].forEach.call(images, function(image) {
                 var originalSrc = image.getAttribute("data-original");
