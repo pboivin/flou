8aba05e704d48f2dc42b5f0752208dc1dc9985ad
Patrick Boivin <pboivin@gmail.com>
2018-10-31 11:20:47 -0400

    Cleanup DefaultImageRendererTest

--- tests/DefaultImageRendererTest.php
+++ tests/DefaultImageRendererTest.php
@@ -1,8 +1,9 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use Flou\Path;
+use Flou\DefaultImageRenderer;
 use Flou\Image;
+use Flou\Path;
 
 
 class DefaultImageRendererTest extends TestCase
@@ -23,17 +24,20 @@ class DefaultImageRendererTest extends TestCase
     {
         $image = (new Flou\Image())
             ->setBasePath(self::$base_path)
-            ->load("image1.jpg")
-            ->process();
-        $this->assertTrue($image->isProcessed());
+            ->load("image1.jpg");
+
+        $renderer = (new DefaultImageRenderer())
+            ->setImage($image)
+            ->setDescription("This is my image");
 
         // Cannot generate HTML without a base_url
-        $html = $image->render("Image Description");
+        $html = $renderer->render();
         $this->assertNull($html);
 
-        $html = $image
-            ->setBaseUrl("/img")
-            ->render("Image Description");
+        $image->setBaseUrl("/img");
+        $html = $renderer->render();
+        $this->assertNotNull($html);
+
         $expected_src = "/img/image1.flou.jpg";
         $expected_data_original = "/img/image1.jpg";
 
@@ -44,46 +48,6 @@ class DefaultImageRendererTest extends TestCase
         $this->assertEquals("flou-image", $container->img['class']);
         $this->assertEquals($expected_src, $container->img['src']);
         $this->assertEquals($expected_data_original, $container->img['data-original']);
-        $this->assertEquals("Image Description", $container->img['alt']);
+        $this->assertEquals("This is my image", $container->img['alt']);
     }
-
-#   /**
-#    * Generate the HTML markup for an image **using custom output settings**
-#    */
-#   public function testRenderCustomOutput()
-#   {
-#       $custom_processed_basepath = self::$custom_processed_basepath;
-#       $custom_processed_path = self::$custom_processed_path3;
-
-#       $image = (new Flou\Image())
-#           ->setBasePath(self::$base_path)
-#           ->load("image1.jpg")
-#           ->setProcessedPath($custom_processed_basepath)
-#           ->setProcessedFile("image1.custom.jpg")
-#           ->process();
-#       $this->assertTrue($image->isProcessed());
-#       $this->assertTrue(file_exists($custom_processed_path));
-
-#       // Cannot generate HTML without a processed_url when using a custom
-#       // output path
-#       $html = $image
-#           ->setBaseUrl("/img")
-#           ->render("Image Description");
-#       $this->assertNull($html);
-
-#       $html = $image
-#           ->setProcessedUrl("/custom")
-#           ->render("Image Description");
-#       $expected_src = "/custom/image1.custom.jpg";
-#       $expected_data_original = "/img/image1.jpg";
-
-#       // The HTML is as expected
-#       $container = new SimpleXMLElement($html);
-#       $this->assertEquals("flou-container", $container['class']);
-#       $this->assertEquals(1, count($container->img));
-#       $this->assertEquals("flou-image", $container->img['class']);
-#       $this->assertEquals($expected_src, $container->img['src']);
-#       $this->assertEquals($expected_data_original, $container->img['data-original']);
-#       $this->assertEquals("Image Description", $container->img['alt']);
-#   }
 }
