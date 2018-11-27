2f5082ff0417a90d743fd4a7ec2605f1797e93f9
Patrick Boivin <pboivin@gmail.com>
2018-11-27 11:08:54 -0500

    Cleanup demos

--- demos/basic.php
+++ demos/basic.php
@@ -1,10 +1,6 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once __DIR__ . "/../vendor/autoload.php";
+require_once "_initialize.php";
 
 use Flou\Image;
 
