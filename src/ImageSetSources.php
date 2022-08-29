2c5d66e9417199d80b56a885c74b120bed8aa160
Patrick Boivin <pboivin@gmail.com>
2022-08-28 20:40:46 -0400

    Update ImageSetSources. Add tests.

--- src/ImageSetSources.php
+++ src/ImageSetSources.php
@@ -2,55 +2,57 @@
 
 namespace Pboivin\Flou;
 
-use InvalidArgumentException;
-
 class ImageSetSources
 {
     protected $imageForLqip = '';
 
-    protected $data = [];
+    protected $preparedSources = [];
 
-    public function __construct(protected array $config, protected ImageSet $imageSet)
-    {
-        $this->acceptConfig($config);
-    }
-
-    protected function acceptConfig(array $config)
+    public function __construct(protected array $config, protected ImageFactory $factory)
     {
         foreach ($config as $source) {
-            if (!isset($source['width'])) {
-                throw new InvalidArgumentException("Source is missing required 'width' option.");
-            }
-
-            if (!isset($source['image']) && !$this->imageSet->image()) {
-                throw new InvalidArgumentException(
-                    "Missing required 'image' option on source or imageset."
-                );
-            }
-
-            $source['image'] ??= $this->imageSet->image();
+            $this->preparedSources[] = $this->prepareSource($source);
 
             $this->imageForLqip = $source['image'];
-
-            $this->data[] = $source;
         }
     }
 
-    public function all(): array
+    protected function prepareSource(array $source): array
     {
-        return array_map(
-            fn ($i) => [
-                'image' => $this->imageSet->factory()->image($i['image'], [
-                    'w' => $i['width'],
-                ]),
-                'width' => "{$i['width']}",
-            ],
-            $this->data
-        );
+        $srcset = [];
+
+        foreach ($source['widths'] as $w) {
+            $srcset[] = [
+                'image' => $this->factory->image($source['image'], ['w' => $w]),
+                'width' => $w,
+            ];
+        }
+
+        $source['srcset'] = $srcset;
+
+        return $source;
+    }
+
+    public function get(): array
+    {
+        return $this->preparedSources;
     }
 
     public function lqip(): Image
     {
-        return $this->imageSet->factory()->image($this->imageForLqip);
+        return $this->factory->image($this->imageForLqip);
+    }
+
+    public function toArray(): array
+    {
+        $data = $this->get();
+
+        foreach ($data as &$source) {
+            foreach ($source['srcset'] as &$item) {
+                $item['image'] = $item['image']->toArray();
+            }
+        }
+
+        return $data;
     }
 }
